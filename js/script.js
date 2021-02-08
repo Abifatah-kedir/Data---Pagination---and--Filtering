96688d045fc636aaefa11dd48cf9b9b5fcc91e85
abifatah <abifatah.kedir>
2021-02-08 08:52:28 -0600

    added list of student and pagination, but have little issue with filtering

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
+const studentList = document.querySelector(".student-list");
+const listPerPage = 8;
 
 /*
 For assistance:
@@ -12,6 +13,19 @@ For assistance:
 */
 
 
+ /*
+      Dynamic Search Bar
+   */
+  const theHeader = document.querySelector(".header");
+  const searchBar = `<label for="search" class="student-search">
+                       <input id="search" placeholder="Search by name...">
+                       
+                       <button type="button">
+                          <img src="img/icn-search.svg" alt = "Search icon">
+                       </button>
+  
+                    </label>`;  
+  theHeader.insertAdjacentHTML(`beforeend`, searchBar);
 
 /*
    Create the `showPage` function
@@ -19,23 +33,23 @@ For assistance:
 */
 
 const showPage = (list, page)=> {
-   let startIndex = (page * 8) - 8;
-   let endIndex = page * 8; 
 
-   const studentList = document.querySelector(".student-list");
+   let startIndex = (page * listPerPage) - listPerPage;
+   let endIndex = page * listPerPage; 
    studentList.innerHTML = "";
-
+  
+  
    for(let i = 0; i < list.length; i++) {
       if( i >= startIndex && i <= endIndex) {
-
-        let listElements = `<li class="student-item cf">
+         let studentData = list[i];
+         let listElements = `<li class="student-item cf">
                                  <div class="student-details">
-                                    <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
-                                    <h3>${list[i].name.last}  ${list[i].name.first}</h3>
-                                    <span class="email">${list[i].email}</span>
+                                    <img class="avatar" src="${studentData.picture.large}" alt="Profile Picture">
+                                    <h3>${studentData.name.last}  ${studentData.name.first}</h3>
+                                    <span class="email">${studentData.email}</span>
                                  </div>
                                  <div class="joined-details">
-                                    <span class="date">Joined ${list[i].registered.date}</span>
+                                    <span class="date">Joined ${studentData.registered.date}</span>
                                  </div>
                               </li>`;
 
@@ -44,46 +58,80 @@ const showPage = (list, page)=> {
    }
 }
 
-
-
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
 const addPagination = (list)=> {
-   const numberOfpagination = Math.ceil(list.length/9);
-
+   const numberOfpagination = Math.floor(list.length/listPerPage);
    const linkList = document.querySelector(".link-list");
    linkList.innerHTML = "";
+   let paginationButtons = "";
 
    for(let i = 1; i <= numberOfpagination; i++) {
-      let paginationButtons = ` <li> <button type="button">${i}</button> </li> `;
-      linkList.insertAdjacentHTML('beforeend', paginationButtons);
-      
+       paginationButtons += `<li> <button type="button">${i}</button> </li>`;
    }
+   linkList.insertAdjacentHTML('beforeend', paginationButtons);
    // const firstPagination = linkList.firstElementChild.firstElementChild;
  
-   let paginationButton = document.querySelectorAll("button");
+   let paginationButton = document.querySelectorAll("li > button");
    paginationButton[0].className = "active";
 
    linkList.addEventListener("click", (event)=> {
-
       let eventTarget = event.target;
-    
       if (eventTarget.tagName === 'BUTTON') {
          eventTarget.className = "active";
          showPage(data, eventTarget.textContent);
       }
-
       for(let i = 0; i < paginationButton.length; i++) {
          paginationButton[i].classList.remove("active");
       }
-
-    
    });
 }
+
+const SearchIcon = document.querySelector(".student-search");
+const valueInput = SearchIcon.querySelector("#search");
+let inputSearch = [];
+
+const  filterdata =  (list, input)=> {
+
+   for(let i = 0; i < list.length; i++)
+   {
+      let student = list[i];
+      let studentNames = student.name.last + " "+ student.name.first;
+      let firstAndLast = studentNames.toLowerCase();
+      if(firstAndLast.includes(input.toLowerCase()) )
+      {
+         inputSearch.push(student);
+         showPage(inputSearch,1);
+         addPagination(inputSearch);  
+      }
+   }
+}
+
+
+SearchIcon.addEventListener('keyup', ()=> {
+   filterdata(data, valueInput.value);
+});
+
+SearchIcon.addEventListener('click', ()=> {
+   filterdata(data, valueInput.value);
+});
+
 
 // Call functions
 showPage(data,1);
 addPagination(data);
+
+
+
+
+
+
+
+
+
+
+
+
