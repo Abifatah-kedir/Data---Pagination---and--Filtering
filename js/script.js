4849f49aca62ab18e925a53963e3c4bbed32fd34
abifatah <abifatah.kedir>
2021-02-10 11:40:40 -0600

    .

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ For assistance:
                        </button>
                     </label>`;  
   theHeader.insertAdjacentHTML(`beforeend`, searchBar);
+
 /*
    Create the `showPage` function
    This function will create and insert/append the elements needed to display a "page" of nine students
@@ -37,6 +38,7 @@ const showPage = (list, page)=> {
    studentList.innerHTML = "";
 
 
+   //generates students by looping through given data.
    for(let i = 0; i < list.length; i++) {
       if( i >= startIndex && i <= endIndex) {
 
@@ -57,8 +59,8 @@ const showPage = (list, page)=> {
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+   Create the `addPagination` function
+   This function will create and insert/append the elements needed for the pagination buttons
 */
 
 const addPagination = (list)=> {
@@ -66,72 +68,97 @@ const addPagination = (list)=> {
    let paginationButtons = "";
    linkList.innerHTML = "";
   
-
    for(let i = 1; i <= numberOfpagination; i++) {
        paginationButtons += `<li> <button type="button">${i}</button> </li>`;
    }
+  
+   // appends generated list in a preceeding order.
    linkList.insertAdjacentHTML('beforeend', paginationButtons);
 
+   //selects first list - applies the class "active".
    let paginationButton = document.querySelectorAll("li > button");
    paginationButton[0].className = "active";
 
+
+   // generates the a new list whenever a button is trickered.
    linkList.addEventListener("click", (event)=> {
+      
       let eventTarget = event.target;
+      // checks the condition,then genarates studentlist.
       if (eventTarget.tagName === 'BUTTON') {
-         eventTarget.className = "active";
-      } 
+         showPage(data, eventTarget.textContent);
+      }
+      // removes all pre-existing "active" classes.
       for(let i = 0; i < paginationButton.length; i++) {
          paginationButton[i].classList.remove("active");
       }
-
-      showPage(data, eventTarget.textContent);
+     
+      // applies active class to the spacified target.
+      eventTarget.className = "active";
    });
  
 }
+
+/* 
+   selects both searchi icon and input.
+*/
+const SearchIcon = document.querySelector(".student-search");
+const valueInput = SearchIcon.querySelector("#search");
+let inputSearch = [];
+
+
+// filters data according to the UserInput.
+const filterdata =  (list, input)=> {
+   // checks the userInput and filters list according given data.
+   if (valueInput.value.length !== 0) {
+      for(let i = 0; i < list.length; i++)
+      {
+         let studentData = list[i];
+         let studentNames = studentData.name.last + " "+ studentData.name.first;
+
+         let firstAndLast = studentNames.toLowerCase();
+         let userInput = input.toLowerCase();
+
+         if( firstAndLast.includes(userInput) )
+         {
+            inputSearch.push(studentData);
+         } 
+      }
+
+      // generates new list and pagination.
+      showPage(inputSearch,1);
+      addPagination(inputSearch);
+     
+   }
+      
+      // Call functions
+      // studentList.innerHTML = `<p class = "no-results">No Results, found </p> `;
+      // linkList.innerHTML = "";
+     
+   
+}
+
+
+// calls filter funtion on keyup event
+SearchIcon.addEventListener('keyup', ()=> {
+   filterdata(data, valueInput.value);
+   // addPagination(data);
+
+});
+
+// calls filter funtion on click event
+SearchIcon.addEventListener("click", (e)=> {
+   e.preventDefault();
+   filterdata(data, valueInput.value);
+})
+
+
+
 
 
 // Call functions
 showPage(data,1);
 addPagination(data);
-
-
-const SearchIcon = document.querySelector(".student-search");
-const valueInput = SearchIcon.querySelector("#search");
-let inputSearch = [];
-
-const filterdata =  (list, input)=> {
-   for(let i = 0; i < list.length; i++)
-   {
-      let studentData = list[i];
-      let studentNames = studentData.name.last + " "+ studentData.name.first;
-
-      let firstAndLast = studentNames.toLowerCase();
-      let userInput = input.toLowerCase();
-
-      if( firstAndLast.includes(userInput) )
-      {
-         inputSearch.push(studentData);
-      } 
-   }
-   
-   if (inputSearch.length > 0 ) {
-      showPage(inputSearch,1);
-      addPagination(inputSearch);
-   } else {
-         studentList.innerHTML = `<p class = "no-results">No Results, found </p> `;
-   }
- 
-}
-
-SearchIcon.addEventListener('keyup', (e)=> {
-   e.preventDefault();
-   filterdata(data, valueInput.value);
-});
-
-// SearchIcon.addEventListener("click", ()=> {
-//    filterdata(data, valueInput.value);
-// })
-
 
 
 
